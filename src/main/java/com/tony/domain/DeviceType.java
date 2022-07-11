50006db4540197015a050f0a83882cbb10a31c92
tony <1605337475@qq.com>
2022-07-11 14:48:52 +0800

    第一次开源

--- src/main/java/com/tony/domain/DeviceType.java
+++
@@ -1,9 +0,0 @@
-package com.tony.domain;
-
-import lombok.Data;
-
-@Data
-public class DeviceType {
-    private String typeName;
-    private String describe;
-}
--- src/main/java/com/tony/domain/MultiSwitchState.java
+++
@@ -1,9 +0,0 @@
-package com.tony.domain;
-
-import lombok.Data;
-
-@Data
-public class MultiSwitchState {
-    private int switchState;
-    private String describe;
-}
